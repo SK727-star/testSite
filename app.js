50c3416f944395c2e1819d44be06df3d3d03b779
SK727-star <kf19980727@gmail.com>
2023-07-17 22:19:49 +0900

    feat: add new tab

--- app.js
+++ app.js
@@ -1,79 +1,36 @@
-const quiz =[
-  { // 1問目
-    question: '太陽系惑星で最も質量の大きい惑星は?',
-    answers: [
-      '火星',
-      '木星',
-      '土星',
-      '太陽'
-    ],
-    correct: '木星'
-  },
 
-  { // 2問目
-    question: '地球から最も近い恒星は?',
-    answers: [
-      'プロキシマケンタウリ',
-      'スピカ',
-      'アルデバラン',
-      'シリウス'
-    ],
-    correct: 'プロキシマケンタウリ'
-  },
+(()=>{
+  const $doc = document;
+  const $tab = $doc.getElementById('js-tab');
+  const $nav = $tab.querySelectorAll('[data-nav]');
+  const $content = $tab.querySelectorAll('[data-content]');
+  const ACTIVE_CLASS = "is-active";
 
-  { // 3問目
-    question: '火星の衛星を選べ',
-    answers: [
-      '月',
-      'ガニメデ',
-      'タイタン',
-      'フォボス'
-    ],
-    correct: 'フォボス'
-  }
+  const init = () => {
+    $content[0].style.display = 'block';
+  };
+  init();
 
-];
-let quizIndex = 0;
-let score     = 0;
+  //クリックしたら起こるイベント
+  const handleClick = (e) => {
+    e.preventDefault();
+    const $this = e.target;
+    const targetValue = $this.dataset.nav;
+    console.log('targetValue', targetValue);
 
+    //コンテンツのリセット
+    for(let i=0;i<$nav.length;i++){
+      $nav[i].classList.remove(ACTIVE_CLASS);
+      $content[i].style.display='none';
+    }
 
-console.log(quiz.length);
-
-const $button = document.getElementsByTagName('button');
-
-const setUpQuiz = () => {
-  document.getElementById('js-question').textContent = quiz[quizIndex].question;
-
-  for(i=0;i < $button.length ;i++){
-    $button[i].textContent = quiz[quizIndex].answers[i];
-  }
-}
-
-setUpQuiz();
-
-
-
-//正誤判定ロジック部。 textContentは上で再定義済み
-const clickHandler = (e) => {
-  if(quiz[quizIndex].correct === e.target.textContent){
-    window.alert('正解');
-    score++;
-  }else{
-    window.alert('不正解');
-  }
-  quizIndex++;
-
-  if(quizIndex < quiz.length){
-    setUpQuiz();
-  }else if(score != 0){
-    window.alert(`終了です。あなたは${quiz.length}問中、${score}問正解しました。`);
-  }else{
-    window.alert(`終了です。あなたは全ての問題に正解できませんでした。`);
-  }
-}
-
-for(i=0;i < $button.length ;i++){ //配置
-  $button[i].addEventListener('click', (e)=>{
-    clickHandler(e);
-  })
-}
+    //対象のコンテンツをアクティブ化
+    $tab.querySelectorAll(`[data-content="${targetValue}"]`)
+    [0].style.display= 'block';
+    $nav[targetValue].classList.add(ACTIVE_CLASS);
+  };
+  //$navの各要素にeventListenerを設定
+  for(let i=0 ;i < $nav.length; i++){
+   $nav[i].addEventListener('click',(e) => handleClick(e));
+ }
+})();
